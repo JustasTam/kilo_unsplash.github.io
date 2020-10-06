90c204be37af0981cceea5639e7cddcf33618944
JustasTam <justas.tamulionis@gmail.com>
2020-10-06 15:15:55 +0300

    make api call, search photos by keyword

--- src/searchPhotos.js
+++ src/searchPhotos.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import Unsplash, { toJson } from "unsplash-js";
+
+const unsplash = new Unsplash({
+	accessKey: process.env.REACT_APP_API_KEY,
+});
 
 export default function SearchPhotos() {
 	const [query, setQuery] = useState("");
 
+	const searchPhotos = async (e) => {
+		e.preventDefault();
+		unsplash.search.photos(query).then(toJson)
+	};
+
   return (
     <>
-    	<form className="form"> 
+    	<form className="form" onSubmit={searchPhotos}> 
         <label className="label" htmlFor="query"> 
           {" "}
           📷
